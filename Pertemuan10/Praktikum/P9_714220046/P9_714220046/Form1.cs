60d498c83886995c25bfcbafd3e6f345241d0e83
gryzlegrizz <gryzlegrizz@gmail.com>
2023-12-06 14:43:53 +0700

    Update

---
+++ Pertemuan10/Praktikum/P9_714220046/P9_714220046/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714220046
+{
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public void Tampil()
+        {
+            //Query DB Get MGS
+            dataMahasiswa.DataSource = koneksi.ShowData("SELECT * FROM t_mahasiswa");
+
+            //Mengubah Nama Kolom Tabel
+            dataMahasiswa.Columns[0].HeaderText = "NPM";
+            dataMahasiswa.Columns[1].HeaderText = "Nama";
+            dataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            dataMahasiswa.Columns[3].HeaderText = "Alamat";
+            dataMahasiswa.Columns[4].HeaderText = "Email";
+            dataMahasiswa.Columns[5].HeaderText = "No HP";    
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Memanggil method Tampil
+            Tampil();
+        }
+    }
+}
